bb1f7cd183981bbd6caff126a377a0e746afc9d6
Newbiedevelope <rbtjr20313@gmail.com>
2023-02-15 19:26:10 +0900

    서블릿 배움

---
+++ src/test/java/hello/servlet/domain/repository/MemberRepositoryTest.java
@@ -0,0 +1,50 @@
+package hello.servlet.domain.repository;
+
+import hello.servlet.domain.member.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        //given
+        Member member = new Member("hello", 20);
+
+        //when
+        Member savedMember = memberRepository.save(member);
+
+        //then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    void findAll() {
+        //given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //when
+        List<Member> result = memberRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+}
